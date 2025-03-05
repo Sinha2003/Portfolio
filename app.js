a1b9f786194978830e3162e108a957612bf23c97
RishiSinha <148899783+Sinha2003@users.noreply.github.com>
2025-03-05 12:31:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+// Feedback Form Submission
+const feedbackForm = document.getElementById('feedback-form');
+const thankYouMessage = document.getElementById('thank-you-message');
+
+feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    thankYouMessage.style.display = 'block';
+    feedbackForm.reset();
+});
+
+
